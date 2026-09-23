503c1918c6c13dc4a6f4650d836e2445381e6eb2
ShinoChan0 <steak014129@gmail.com>
2026-09-24 01:34:33 +0900

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ shared_script '@fiveguard/ai_module_fg-obfuscated.lua'
 fx_version 'cerulean'
 game 'gta5'
 
-author 'YourName'
+author 'ShinoChan'
 description 'Fix minimap to north with a command'
 version '1.0.0'
 
